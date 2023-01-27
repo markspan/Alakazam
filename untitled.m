c6ec4b4a47e2845c27b0508af9e388063b4223ce
Mark M. Span <m.m.span@gmail.com>
2023-01-27 11:45:36 +0100

    r

--- untitled.m
+++ untitled.m
@@ -1,4 +1,20 @@
- Fs = 44.1e3; N = 1000;
- x = sin(2*pi*(1:N)/N + (10*(1:N)/N).^2);
- findpeaks(x,Fs, ...
-           'Annotate','extents')
+d = AlakazamInst.Workspace.EEG;
+data = d.data(:);
+halfway = mean([max(data), min(data)]);
+dist =  abs(data - halfway);
+[peaks, indices] = findpeaks(-dist,'Npeaks',176, "MinPeakDistance",950);
+indices = indices(2:end);
+
+for b = 0:7
+    indices([1+(b*22)]) = nan;
+end
+
+idx = indices(~isnan(indices));
+idx = idx(1:end-1)
+
+stimevents = d.event(strcmpi({d.event.code},'Stimulus'));
+idxevt = [stimevents.latency]';
+
+difference = idx-idxevt;
+dat = table(idx, idxevt, difference);
+
