f7b4a23f37edb05a24619e46d30542fdd3a50b4d
Mark M. Span <m.m.span@gmail.com>
2023-06-23 11:17:02 +0200

    ica

---
+++ Transformations/Decompose ICA/DecomposeICA.m
@@ -0,0 +1,28 @@
+function [pfigure, ropts] = DecomposeICA(EEG,~)
+%% Create a poincare plot for the IBI series
+% The ibis are plotted agains a time-delayed version of the same values. If
+% the 'bylabel' option is used, the plot has different partitions for each
+% value the label takes on.
+
+%#ok<*AGROW>
+ropts = 'graph';
+%% Check for the EEG dataset input:
+if (nargin < 1)
+    ME = MException('Alakazam:DecomposeICA','Problem in DecomposeICA: No Data Supplied');
+    throw(ME);
+end
+
+[~, name, ~]= fileparts(EEG.File);
+
+%pfigure = figure('Name', name, 'Visible', false, 'Units', 'normalized');
+%figure(pfigure)
+EEG.id = ['timefreq:' name];
+EEG = pop_runica(EEG);
+EEG = pop_chanedit(EEG);
+EEG=iclabel(EEG, 'beta');
+
+pop_viewprops(EEG,0);
+
+pfigure = gcf;
+set(gcf, 'visible', false)
+
---
+++ Transformations/RemoveComp/RemoveComp.m
@@ -0,0 +1,22 @@
+function [EEG, opts] = RemoveComp(EEG,~)
+%% Create a poincare plot for the IBI series
+% The ibis are plotted agains a time-delayed version of the same values. If
+% the 'bylabel' option is used, the plot has different partitions for each
+% value the label takes on.
+
+%#ok<*AGROW>
+ropts = 'graph';
+%% Check for the EEG dataset input:
+if (nargin < 1)
+    ME = MException('Alakazam:RemoveComp','Problem in RemoveComp: No Data Supplied');
+    throw(ME);
+end
+if (nargin == 1)
+    opts = 'Init';
+end
+
+[~, name, ~]= fileparts(EEG.File);
+
+EEG.id = ['timefreq:' name];
+EEG = pop_subcomp(EEG);
+
